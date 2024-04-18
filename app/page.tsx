d0a3df96daaa08eacdf090b2f7843402a7d356bb
Zach Raab <zpraab@gmail.com>
2024-04-18 00:29:47 -0600

    Add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,57 @@
-import Image from "next/image";
+import myImage from "../public/DSC_1344.jpg"
 
 export default function Home() {
+  const aboutStyle = { margin: "20px" };
+  const projectStyle = { margin: "20px" };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <nav
+      className="main-header-menu"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: 40,
+      }}
+      >
+        <section style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <h1
+            style={{
+              fontSize: "4rem",
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Zach Raab{"  "}
+            <span style={{ fontSize: "2rem" }}>Full-Stack Developer</span>
+          </h1>
+            <img
+              style={{ objectPosition: "top center", objectFit: "cover", height: "300px", width: "300px" }}
+              src={myImage.src} alt="Headshot of Zach Raab."
+            ></img>
+        </section>
+        <section
+          style={{
+            display: "flex",
+            fontSize: "2rem",
+          }}
+        >
+        <div>
+          <a href="https://www.linkedin.com/in/zachary-raab-61578248/">
+            LinkedIn
           </a>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+        <div>
+          <a href="https://github.com/zachraab">GitHub</a>
+        </div>
+        </section>
+      </nav>
+      <section style={aboutStyle}>
+        <h2>About Me</h2>
+        <p>
+        A versatile full-stack developer with a background spanning entertainment, and audio engineering. Proficient in cultivating creativity and resolving complex challenges, I excel in collaborative team settings. Committed to continuous learning and adept at adopting emerging technologies. My upbringing amidst the natural splendor of Rocky Mountain National Park instilled a deep appreciation for the outdoors. 
+        </p>
+      </section>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
+        {/* <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -56,7 +67,6 @@ export default function Home() {
             Find in-depth information about Next.js features and API.
           </p>
         </a>
-
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -106,7 +116,7 @@ export default function Home() {
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a>
+        </a> */}
       </div>
     </main>
   );
