d1b485dc8a8a7d4255025520b406b6b74732b5e5
Zach Raab <zpraab@gmail.com>
2024-04-18 01:17:45 -0600

    Update content

--- app/page.tsx
+++ app/page.tsx
@@ -1,123 +1,47 @@
-import myImage from "../public/DSC_1344.jpg"
+import myImage from "/DSC_1344.jpg"
 
 export default function Home() {
-  const aboutStyle = { margin: "20px" };
-  const projectStyle = { margin: "20px" };
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav
-      className="main-header-menu"
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        margin: 40,
-      }}
-      >
-        <section style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <h1
-            style={{
-              fontSize: "4rem",
-            }}
-          >
-            Zach Raab{"  "}
-            <span style={{ fontSize: "2rem" }}>Full-Stack Developer</span>
-          </h1>
-            <img
-              style={{ objectPosition: "top center", objectFit: "cover", height: "300px", width: "300px" }}
-              src={myImage.src} alt="Headshot of Zach Raab."
-            ></img>
-        </section>
-        <section
-          style={{
-            display: "flex",
-            fontSize: "2rem",
-          }}
-        >
-        <div>
-          <a href="https://www.linkedin.com/in/zachary-raab-61578248/">
-            LinkedIn
-          </a>
-        </div>
-        <div>
-          <a href="https://github.com/zachraab">GitHub</a>
-        </div>
-        </section>
-      </nav>
-      <section style={aboutStyle}>
-        <h2>About Me</h2>
-        <p>
-        A versatile full-stack developer with a background spanning entertainment, and audio engineering. Proficient in cultivating creativity and resolving complex challenges, I excel in collaborative team settings. Committed to continuous learning and adept at adopting emerging technologies. My upbringing amidst the natural splendor of Rocky Mountain National Park instilled a deep appreciation for the outdoors. 
-        </p>
-      </section>
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        {/* <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const aboutStyle = { margin: "20px" };
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<nav
+			className="main-header-menu"
+			style={{
+				display: "flex",
+				justifyContent: "space-between",
+				alignItems: "center",
+				margin: 40,
+			}}
+			>
+				<section style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+					<h1 style={{ fontSize: "4rem" }}>
+						Zach Raab{"  "}<span style={{ fontSize: "2rem" }}>Full-Stack Developer</span>
+					</h1>
+						<img
+							style={{ objectPosition: "top center", objectFit: "cover", height: "300px", width: "300px" }}
+							src={myImage.src} alt="Headshot of Zach Raab."
+						></img>
+				</section>
+				<section style={{ display: "flex", fontSize: "2rem", }}>
+					<div>
+						<a href="https://www.linkedin.com/in/zachary-raab-61578248/">
+							LinkedIn
+						</a>
+					</div>
+					<div>
+						<a href="https://github.com/zachraab">GitHub</a>
+					</div>
+				</section>
+			</nav>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+			<section style={aboutStyle}>
+				<h2>About Me</h2>
+				<p>
+				Zach is a versatile full-stack developer with a diverse background spanning music, dance, audio engineering, and programming. His upbringing amidst the natural splendor of Rocky Mountain National Park instilled a deep appreciation for the outdoors. During his tenure at Blennd, he developed custom wordpress websites that garnered acclaim from both users and industry experts. Proficient in cultivating creativity and resolving complex challenges, he excels in collaborative team settings. His role involved collaborating with cross-functional teams, including designers and content creators, to deliver seamless user experiences. Committed to continuous learning, he has completed multiple online courses in emerging technologies such as Typescript and React, further enhancing his skill set.
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a> */}
-      </div>
-    </main>
-  );
+				As a React developer, Zach brings a robust skill set tailored to crafting dynamic and responsive web applications. Proficient in Next.js, he has leveraged its server-side rendering capabilities to optimize performance and enhance user experience. His expertise in React enables him to architect scalable and maintainable front-end solutions, ensuring seamless interaction and intuitive interfaces. Additionally, Zach is well-versed in TypeScript, utilizing its strong typing system to catch errors early in the development process and improve code reliability. His proficiency in Tailwind CSS allows him to efficiently style components and design visually appealing layouts while maintaining flexibility and consistency across projects. With a keen eye for detail and a passion for clean, modular code, Zach excels in driving projects forward with precision and innovation.
+				</p>
+			</section>
+		</main>
+	);
 }
