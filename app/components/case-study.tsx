7a2ca0a019768488c1eb9a306861b2505272fc82
Zach Raab <zpraab@gmail.com>
2024-05-29 10:44:21 -0600

    Stack modals on mobile view

--- app/components/case-study.tsx
+++ app/components/case-study.tsx
@@ -16,14 +16,14 @@ export default function CaseStudy({ darkMode, bgColor, name, url, img, descripti
 				</div>
 				<ModalContents darkMode={ darkMode ? true : false }>
 					<div 
-						className="flex flex-row justify-center items-center p-16 space-x-8"
+						className="flex flex-col md:flex-row justify-center items-center p-16 space-y-4 md:space-y-0 md:space-x-8"
 						style={{ 
 							backgroundColor: bgColor,
 							color: 	darkMode ? 'white' : ''
 						}}
 					>
 
-						<div className="flex flex-col w-1/3 justify-center items-center">
+						<div className="flex flex-col md:w-1/3 justify-center items-center">
 							<Image 
 								src={img.src}
 								alt={`Logo of ${name}`}
@@ -34,9 +34,9 @@ export default function CaseStudy({ darkMode, bgColor, name, url, img, descripti
 							<br/>
 							<a className="relative inline-block text-left w-fit before:-skew-x-12 before:-m-1 before:ease before:duration-300 before:bg-gray-300 before:absolute before:left-0 before:bottom-0 before:translate-x-1/4 before:h-1/4 before:w-0 hover:before:w-full active:before:bg-gray-600" href={url} target="_blank" rel='nofollow'><span className='relative'>Check it out!</span></a>
 						</div>
-						<div className="flex flex-col justify-center w-2/3">
+						<div className="flex flex-col justify-center md:w-2/3">
 							<h3 className="font-bold text-xl">{name}</h3>
-								<hr className="w-2/3"/>
+								<hr className="md:w-2/3"/>
 							<p>{description}</p>
 							<br/>
 							<p className='text-xs'>Credit: Blennd</p>
