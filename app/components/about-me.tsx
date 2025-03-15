625d61189ed9d58748548b9372c1d23924480436
Zach Raab <zpraab@hotmail.com>
2025-03-14 18:01:53 -0600

    Update about me

--- app/components/about-me.tsx
+++ app/components/about-me.tsx
@@ -8,11 +8,11 @@ export default function AboutMe() {
 				<span className="relative bg-inherit">About</span>
 			</h2>
 				<hr className="w-full"/>
+			<p data-aos="fade-right" data-aos-delay="100">Zach is a versatile full-stack developer. He creates custom websites and is skilled at fostering creativity while solving complex challenges. He thrives in collaborative team settings, working closely with designers and content creators to deliver seamless user experiences.</p>
+				<br/>
 			<p data-aos="fade-right">
 				Zach&apos;s upbringing amidst the natural splendor of Rocky Mountain National Park instilled in him a deep appreciation for the outdoors. He spent most weekends roaming the trails of Deer Creek Canyon. Zach and his wife, Erika, are professional ballroom dancers and dance instructors. They have been dancing and teaching together for three years while traveling to compete across the western United States. From a young age, Zach had a passion for music as a saxophonist and later pursued the study of jazz and music composition. He is also a DJ and has been in the business for nine years, providing entertainment for weddings and parties throughout Colorado.
 			</p>
-				<br/>
-			<p data-aos="fade-right" data-aos-delay="100">In addition to his diverse background, Zach is a versatile full-stack developer. He creates custom WordPress websites and is skilled at fostering creativity while solving complex challenges. He thrives in collaborative team settings, working closely with designers and content creators to deliver seamless user experiences.</p>
 		</section>
 	)
 }
