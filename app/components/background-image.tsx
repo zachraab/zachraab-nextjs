52148e07ce4d4e6f850b3ce80213eb38080ad067
Zach Raab <zpraab@hotmail.com>
2025-03-20 17:39:26 -0600

    Conditionally add overlay class to background-image

--- app/components/background-image.tsx
+++ app/components/background-image.tsx
@@ -1,7 +1,7 @@
 export default function BackgroundImage({imageFileName, headline}: {imageFileName:string, headline?:string}) {
 
     return (
-        <div className="parallax flex justify-center items-center" style={{ backgroundImage: `url(/${imageFileName})` }}>
+        <div className={`parallax ${ headline && 'overlay '}flex justify-center items-center`} style={{ backgroundImage: `url(/${imageFileName})` }}>
            
            {headline && ( 
                 <h2 className="z-[2] text-2xl md:text-6xl lg:text-8xl font-bold text-white">{headline}</h2> 
